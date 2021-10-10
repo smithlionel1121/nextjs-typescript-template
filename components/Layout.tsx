1a3e50d6c8b9dbbdb926596e1255f1d1362735b3
Lionel Smith <smithlionel1121@gmail.com>
2021-10-10 21:44:11 +0100

    Add navbar component to layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,10 +1,16 @@
 import { ReactElement } from 'react';
+import Navbar from './Navbar';
 
 type Props = {
   children: ReactElement;
 };
 const Layout = ({ children }: Props) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Navbar />
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
